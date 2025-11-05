1925b617fcf3329a95d1e6a7930e3bf3547c74ed
Zhefitra24 <zhefitraw@gmail.com>
2025-11-05 23:22:15 +0700

    tambah main copp

---
+++ 07_toupper.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string teks = "Aku Anak RPL";
+
+    cout << "HURUF BESAR: ";
+    for (char c : teks)
+        cout << (char)toupper(c);
+    cout << endl;
+
+    cout << "huruf kecil: ";
+    for (char c : teks)
+        cout << (char)tolower(c);
+    cout << endl;
+
+    return 0;
+}
